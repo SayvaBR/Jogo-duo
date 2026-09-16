9a24d597e4eda26d89b6c24e11ebcc2c77ce9472
SayvaBR <saypvpgames@gmail.com>
2026-09-16 16:17:31 -0300

    fix: close React root JSX element for Android build

--- src/main.tsx
+++ src/main.tsx
@@ -4,4 +4,4 @@ import App from './App';
 import './styles.css';
 import './visual-upgrade.css';
 
-createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode);
+createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
